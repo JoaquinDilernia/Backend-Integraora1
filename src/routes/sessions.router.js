5e19020767b9c45e6df91997126701a9c0f67052
joacodiler <jdilernia99@gmail.com>
2023-08-22 14:51:26 -0300

    entrega

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,54 +1,15 @@
 import { Router } from "express";
-import { userModel } from "../dao/mongo/models/user.model.js";
-
-// Passport
-import passport from "passport";
+import sessionsController from "../controllers/sessions.controller.js";
+import passport from 'passport';
 
 const sessions = Router();
 
-// Endpoint para loguearse:
-sessions.post("/login", passport.authenticate('login'), async (req, res) => {
-	try {
-		const email = req.user.email;
-		await userModel.findOne({email});
-		req.session.user = {
-			first_name: req.user.first_name,
-			last_name: req.user.last_name,
-			email: req.user.email,
-		};
-		return res.status(200).send({status: 'success', response: 'User loged'});
-	} catch (err) {
-		return res.status(500).json({ error: err.message });
-	};
-});
-
-// Endpoint para registrarse:
-sessions.post("/register", passport.authenticate("register"), async (req, res) => {
-	try {
-		req.session.user = {
-			first_name: req.user.first_name,
-			last_name: req.user.last_name,
-			email: req.user.email,
-		};
-		return res.status(200).send({status: 'success', response: 'User created'});
-	} catch (err) {
-		return res.status(500).json({ status: 'error', response: err.message });
-	};
-});
-
-// Endpoint para desloguearse:
-sessions.post("/logout", (req, res) => {
-	try {
-		req.session.destroy((err) => {
-			if (!err) {
-				return res.status(200).send(`Loged out`);
-			};
-
-			return res.status(500).send({ status: `Logout error`, payload: err });
-		});
-	} catch (err) {
-		return res.status(500).json({ error: err.message });
-	};
-});
+sessions.post("/login", passport.authenticate('login'), sessionsController.login);
+sessions.post("/loginjwt", passport.authenticate('jwt'), sessionsController.loginjwt);
+sessions.post("/register", passport.authenticate('register'), sessionsController.register);
+sessions.post("/logout", sessionsController.logout);
+sessions.post("/current", passport.authenticate('register'), sessionsController.current);
+sessions.get("/github", passport.authenticate('github'), sessionsController.github);
+sessions.get("/githubCallback", passport.authenticate('github'), sessionsController.githubCallback);
 
 export default sessions;
