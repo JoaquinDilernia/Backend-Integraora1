9e3288192c1a46abd930becd5edf64147f156f64
joacodiler <jdilernia99@gmail.com>
2023-09-07 09:31:26 -0300

    entregas

--- src/app.js
+++ src/app.js
@@ -1,60 +1,57 @@
 import express from 'express';
 import session from 'express-session';
 import handlebars from 'express-handlebars';
-import router from './routes/router.js';
-import config from './config/enviroment.config.js';
-import initializePassport from './config/passport.config.js';
-import __dirname from './utils.js';
+import compression from 'express-compression';
 import MongoStore from 'connect-mongo';
-import mongoose from 'mongoose';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import cors from "cors";
+import __dirname from './directory.js';
+import router from './routes/router.js';
+import setupSocket from './utils/socket.utils.js';
+import config from './config/environment.config.js';
+import initializePassport from './config/passport.config.js';
+import logger from './utils/logger.util.js';
 
-// Enviroment
 const mongoUrl = config.MONGO_URL;
 const mongoSessionSecret = config.MONGO_URL;
 const cookieSecret = config.COOKIE_SECRET;
 const PORT = config.PORT;
 const HOST = config.HOST;
 
-// App
 const app = express();
-
-// Mongo
-const enviroment = async () => {
-	await mongoose.connect(mongoUrl);
-};
-enviroment();
 initializePassport();
+
 app.use(
 	session({
 		store: MongoStore.create({ mongoUrl }),
 		secret: mongoSessionSecret,
 		resave: false,
-		saveUninitialized: true,
+		saveUninitialized: false,
 	})
 );
-
-// Passport
+app.use(compression({
+  brotli: {
+    enable: true,
+    zlib: {}
+  }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Handlebars
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(cookieSecret));
+app.use(morgan('dev'));
+app.use(cors());
 
-// Server
-app.listen(PORT, HOST, () => {
-	console.log(`Server up on http://${HOST}:${PORT}`);
+const httpServer = app.listen(PORT, HOST, () => {
+	logger.info(`Server up on http://${HOST}:${PORT}`)
 });
+setupSocket(httpServer);
 
 router(app);
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,18 +1,15 @@
-import authController from "../controllers/auth.controller.js";
-import adminController from "../controllers/admin.controller.js";
-import viewsRoute from './views.router.js';
-import sessionsRoute from './sessions.router.js';
-import productsRoute from './products.router.js';
-import cartsRoute from './carts.router.js';
+import views from './views.router.js';
+import sessions from './sessions.router.js';
+import products from './products.router.js';
+import carts from './carts.router.js';
+import logs from './logs.router.js'
 
 const router = (app) => {
-  app.use("/auth", authController);
-  app.use("/admin", adminController);
-
-  app.use("/", viewsRoute);
-  app.use("/api/sessions", sessionsRoute);
-  app.use("/api/products", productsRoute);
-  app.use("/api/carts", cartsRoute);
+	app.use('/', views);
+	app.use('/api/sessions', sessions);
+	app.use('/api/products', products);
+	app.use('/api/carts', carts);
+	app.use('/api/loggerTest', logs)
 };
 
 export default router;
